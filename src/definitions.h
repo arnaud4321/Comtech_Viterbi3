868b41f430c502e833501ac65ad8b32ca425efab
gsorin <soring0412@gmail.com>
2026-01-15 15:56:43 +0200

    Additional definitions

--- src/definitions.h
+++ src/definitions.h
@@ -4,6 +4,8 @@ const int BatchSize3 = 3 * BatchSize1;
 const int BatchSize1Bytes = BatchSize1 >> 3;
 const int BatchConvSize1 = BatchSize1 * 2;
 const int TxOutputBatchSize = BatchSize3 * 4;
+const int ReceiverInputBatchIQSamples = BatchSize3 * 2; //BatchSize3 x2 (conv) /2 (IQ) x2 (2x)
+const int ReceiverInputBatchIQSymbols = BatchSize3;
 const int SPB = 8192;
 const double SamplingFrequency = 21.42e6;
 
