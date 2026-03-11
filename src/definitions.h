f2a90c1628f92c35002e09552f802c680bee4f5c
gsorin <soring0412@gmail.com>
2026-03-11 13:45:18 +0200

    Additional

--- src/definitions.h
+++ src/definitions.h
@@ -1,9 +1,12 @@
 #pragma once
+const double SymbolRate = 10.71e6;
+const double SamplingRate = 2.0*SymbolRate;
 const int BatchSize1 = 1024;
 const int BatchSize3 = 3 * BatchSize1;
 const int BatchSize1Bytes = BatchSize1 >> 3;
 const int BatchConvSize1 = BatchSize1 * 2;
 const int TxOutputBatchSize = BatchSize3 * 4;
+const double TxOutputBatchDuration = (double) TxOutputBatchSize/ SamplingRate;
 const int ReceiverInputBatchIQSamples = BatchSize3 * 2; //BatchSize3 x2 (conv) /2 (IQ) x2 (2x)
 const int ReceiverInputBatchIQSymbols = BatchSize3;
 const int SPB = 8192;
